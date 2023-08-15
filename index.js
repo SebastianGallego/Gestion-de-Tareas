75657c4e93e9a9a094036683da2c2baacc8a3037
Sebastian Gallego <sebastian.gallego@gmail.com>
2023-08-15 19:01:36 -0300

    inicio del programa

---
+++ index.js
@@ -0,0 +1,21 @@
+
+    function irAversion1() {
+        window.location.href = 'version1.html';
+        }
+    
+
+    function irAversion2() {
+        window.location.href = 'version2.html';
+        }
+    
+
+
+
+    
+
+
+
+
+      
+
+   
