7642fc994c6cbb7cabfc4a664a02be0a220a2f5e
Sebastian Gallego <sebastian.gallego@gmail.com>
2023-08-15 22:51:10 -0300

    menu

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
+let tareasPendientes = [];
+let tareasCompletadas = [];
+let menuIngresado ; 
 
-    function irAversion1() {
-        window.location.href = 'version1.html';
-        }
+
+function ejecutarTareas() {
+
+  mostrarMenu();  
+  menuIngresado =  prompt("ingrese una opción de Menú").toLowerCase(); 
+ 
+
+  while (menuIngresado != "e") {  
+     
+    console.clear();
+    mostrarMenu();  
+    menuIngresado = prompt("ingrese una opción de Menú").toLowerCase(); 
     
-
-    function irAversion2() {
-        window.location.href = 'version2.html';
-        }
-    
-
+    }
 
 
     
+}
 
 
+function mostrarMenu() {
+    console.log('Menú: Gestión de Tareas');
+    console.log('a - Agregar nueva tarea');
+    console.log('b - Marcar tarea como completada');
+    console.log('c - Listar tareas pendientes');
+    console.log('d - Listar tareas completadas');
+    console.log('e - Salir del programa');
+  }
 
 
       
